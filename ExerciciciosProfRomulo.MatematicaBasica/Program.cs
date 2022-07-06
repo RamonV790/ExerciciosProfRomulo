6bdb96041f9e8280fb403028362b1eab48b4da7c
RamonV790 <ramon.miranda@aluno.senai.br>
2022-07-06 11:35:27 -0300

    Atividades sobre boletim

---
+++ ExerciciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 3- No projeto MatematicaBasica crie um 
+               Programa que calcule a soma de dois números inteiros.
+               Considere que o usuário vai digitar esses dois números.
+             */
+            int num1;
+            int num2;
+            int soma;
+            //entrada de dados
+            Console.WriteLine("jovem jovem, digite o primeiro numero ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Jovem jovem, digite o segundo numero ");
+            num2 = int.Parse(Console.ReadLine());
+            soma = num1 + num2;
+            Console.WriteLine("A soma desses numeros é: "+ soma);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ExercicioProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nt1;
+            double nt2;
+            double nt3;
+            double soma;
+            double media;
+            string nome;
+            double nfinal;
+            double mfinal;
+            Console.WriteLine("Nome do Aluno: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Nota do primeiro bimestre: ");
+            nt1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota do segundo bimestre: ");
+            nt2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota do terceiro bimestre: ");
+            nt3 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Aluno: {nome}");
+            soma = nt1 + nt2 + nt3;
+            nfinal = Math.Round(soma, 2, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"Soma das notas: {nfinal}");
+           media = soma / 3;
+            mfinal = Math.Round(media, 2, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"Média das notas: {mfinal}");
+            Console.ReadLine();
+            
+           
+
+        }
+    }
+}
